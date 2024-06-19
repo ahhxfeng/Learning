8f4daae6bdb16b8c2809f9aa0fa5dc11eb8d894a
TF <ahhxfeng@gmail.com>
2024-06-19 16:16:20 +0800

    add more test slice

--- demo/example_go/sort/sort_test.go
+++ demo/example_go/sort/sort_test.go
@@ -5,6 +5,13 @@ import "testing"
 func TestBubble(t *testing.T) {
 	t.Log("Test bubble sort start !!")
 	list1 := IntSlice{1, 3, 5, 8, 4, 6, 9, 7, 10}
+	list2 := IntSlice{5, 4, 4, 6, 3, 7, 9, 11, 43, 21, 66, 100}
+	list3 := IntSlice{0, 1, 5, 8, 11, 9, 9, 9, 8, 9, 11, 77}
 	list1.bubbleSort()
-	t.Logf("sorted slice %d", list1)
+	list2.bubbleSort()
+	list3.bubbleSort()
+	t.Logf("sorted slice list1: %d", list1)
+	t.Logf("sorted slice list2: %d", list2)
+	t.Logf("sorted slice list3 %d", list3)
+
 }
